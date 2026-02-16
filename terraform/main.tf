d64741ec08e9e8d2a6ad570e64b564dca9aedde5
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-02-16 20:52:22 +0100

    Adjusted rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,18 @@ resource "azurerm_network_security_group" "vm_nsg" {
   }
 
   security_rule {
+    name                       = "Port-6000-6500"
+    priority                   = 980
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "6000-6100"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
     name                       = "Allow-Internet-Outbound"
     priority                   = 4090
     direction                  = "Outbound"
