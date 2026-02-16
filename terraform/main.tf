3b0639d04a91341ea7cbd809b81dbbe0dc53fbd5
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-02-16 19:44:28 +0100

    Updated with NSG

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,45 @@ resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+
+# Network Security Group
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "nsg-homelab-${var.environment}-weu"
+  location            = data.azurerm_resource_group.default_resource_group.location
+  resource_group_name = data.azurerm_resource_group.default_resource_group.name
+
+  security_rule {
+    name                       = "Allow-SSH-From-Trusted-IP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "87.104.29.3"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Internet-Outbound"
+    priority                   = 65000
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
+}
+
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+
 resource "azurerm_network_interface" "nic" {
   name                = "vm-nic"
   location            = data.azurerm_resource_group.default_resource_group.location
