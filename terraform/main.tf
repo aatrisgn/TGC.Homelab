cba73ac682ae0b088147b4a6e324f8f0884c50de
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-03-21 18:36:03 +0100

    Moving vnet reference to pool

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,8 +56,8 @@ resource "azurerm_lb_backend_address_pool" "frp_backend_pool" {
 resource "azurerm_lb_backend_address_pool_address" "frp_backend_pool_address" {
   name                    = "frp-backend-address"
   backend_address_pool_id = azurerm_lb_backend_address_pool.frp_backend_pool.id
-  virtual_network_id      = azurerm_virtual_network.vnet.id
-  ip_address              = azurerm_linux_virtual_machine.vm.private_ip_address
+  //virtual_network_id      = azurerm_virtual_network.vnet.id
+  ip_address = azurerm_linux_virtual_machine.vm.private_ip_address
 }
 
 resource "azurerm_lb_probe" "http_probe" {
