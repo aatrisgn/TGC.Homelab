67dd13a35c356c2af89f81768e31b01decf58644
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-03-21 18:43:45 +0100

    Switched to manual

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ resource "azurerm_lb_backend_address_pool" "frp_backend_pool" {
   loadbalancer_id    = azurerm_lb.default.id
   name               = "FrpBackEndAddressPool"
   virtual_network_id = azurerm_virtual_network.vnet.id
-  synchronous_mode   = "Automatic"
+  synchronous_mode   = "Manual"
 }
 
 resource "azurerm_lb_backend_address_pool_address" "frp_backend_pool_address" {
