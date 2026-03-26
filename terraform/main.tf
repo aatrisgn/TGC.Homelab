83ac86f01c4aea5a0c7d09a2731e6c46f16c629f
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-03-26 21:24:23 +0100

    Ensured unique naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,7 +111,7 @@ resource "azurerm_lb_rule" "http_rule" {
 
 resource "azurerm_lb_rule" "frp_rule" {
   loadbalancer_id                = azurerm_lb.default.id
-  name                           = "http"
+  name                           = "frp"
   protocol                       = "Tcp"
   frontend_port                  = 7500
   backend_port                   = 7500
