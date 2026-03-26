5a132444d0ebc7c1c2b015bd9c25d66d483227d7
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-03-26 19:39:35 +0100

    Fixing

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,7 +149,7 @@ resource "azurerm_network_security_group" "vm_nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "7500"
-    source_address_prefix      = azurerm_public_ip.lb_pip.ip_address
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
