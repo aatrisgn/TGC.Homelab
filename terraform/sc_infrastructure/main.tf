a58b9211ed4d4b8c9cd68b69eeb4bb5516a59fe6
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-07-05 18:04:35 +0200

    format, lol

--- terraform/sc_infrastructure/main.tf
+++ terraform/sc_infrastructure/main.tf
@@ -32,5 +32,5 @@ resource "scaleway_vpc_private_network" "pn_priv" {
 }
 
 resource "scaleway_instance_ip" "ip" {
-    Project_id = data.scaleway_account_project.default_project.id
+  Project_id = data.scaleway_account_project.default_project.id
 }
