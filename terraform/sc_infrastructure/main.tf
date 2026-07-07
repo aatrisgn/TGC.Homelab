da73885b8f138da4273861a44c7eff59e59dafc5
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-07-07 19:34:09 +0200

    adding another domain

--- terraform/sc_infrastructure/main.tf
+++ terraform/sc_infrastructure/main.tf
@@ -1,9 +1,3 @@
-# resource "scaleway_domain_zone" "domain_zone" {
-#   domain     = var.environment == "prd" ? "tgcportal.com" : "dev.tgcportal.com"
-#   subdomain  = "homelab"
-#   project_id = data.scaleway_account_project.default_project.id
-# }
-
 resource "scaleway_domain_record" "homelab_a_records" {
   for_each = toset(local.domain_records)
 
