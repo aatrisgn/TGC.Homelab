704f165771c8e02d2a06869981ea49641cebbfe9
aatrisgn <49903629+aatrisgn@users.noreply.github.com>
2026-07-11 21:15:19 +0200

    Removed things

--- terraform/data.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_client_config" "current" {}
-
-data "azurerm_resource_group" "default_resource_group" {
-  name = var.resource_group_name
-}
--- terraform/modules/bastion/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_bastion_host" "bastion" {
-  name                = "bas-bastion-${var.environment}-${var.location_shortcode}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  virtual_network_id  = var.virtual_network_id
-
-  sku                = "Developer"
-  copy_paste_enabled = true
-}
